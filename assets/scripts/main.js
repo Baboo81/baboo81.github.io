7da31c64a08da646f9d5f2aa997098890bea79cb
Chris Rodriguez Perez <magicbaboo@gmail.com>
2023-05-13 20:29:21 +0200

    main.js -> json

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+DATA = [
+    {
+        "title" : "Molenbeek en promenade",
+        "img" : "./img/molenbeek.png",
+        "txt" : "Molenbeek en promenade propose une série de parcours à travers cette jolie commune en passant pasr des chemins bucoliques. Le site permet de visualiser les parcours ainsi que la distance de chaque parcours.",
+        "title2" : "TECHNOLOGIES", 
+    }
+]
