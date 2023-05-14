9c60476905dc3c75c2de267b2374a22d9ff8b6ce
Chris Rodriguez Perez <magicbaboo@gmail.com>
2023-05-14 18:43:42 +0200

    main.js script

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,20 @@
 DATA = [
     {
+        //Card 1:
         "title" : "Molenbeek en promenade",
         "img" : "./img/molenbeek.png",
         "txt" : "Molenbeek en promenade propose une série de parcours à travers cette jolie commune en passant pasr des chemins bucoliques. Le site permet de visualiser les parcours ainsi que la distance de chaque parcours.",
-        "title2" : "TECHNOLOGIES", 
+        "title2" : "TECHNOLOGIES",
+        "svg" : "./img/svg/PictoMolen.svg",
+        "btn" : "" 
+    },
+    {
+        //Card 2:
+        "title" : "Hackathon 2023",
+        "img" : "./img/spookyCode.png",
+        "txt" : "Challenge réalisé en trois jours avec la collaboration de quatre collègues. Le site contient un jeu, dont le but est de ramasser une série d'objet avec le personnage.",
+        "title2" : ""
+
     }
+
 ]
