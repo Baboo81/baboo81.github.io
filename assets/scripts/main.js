da46c31a35c43ea9edb19f65ca19dd2186b6fa8a
Baboo81 <magicbaboo@gmail.com>
2023-12-20 13:24:50 +0100

    New version

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,123 +1,41 @@
-//Pour exécuter le js une fois que le document est prêt
-//jQuery styles:
-jQuery(document).ready(function(){
-    jQuery('#logo').css({
-        position: 'relative',
-        opacity: '100%',
-        height: '300px',
-        width: '300px',
-        left: '80%',
-        bottom: '40%',
-        });
-});
+"use strict";
 
-//JSON contenant les informations des différentes cartes:
-const DATA = [
-    {
-        //Card 1:
-        "title" : "Molenbeek en promenade",
-        "img" : "./img/molenbeek.png",
-        "txt" : "Molenbeek en promenade propose une série de parcours à travers cette jolie commune en passant pasr des chemins bucoliques. Le site permet de visualiser les parcours ainsi que la distance de chaque parcours.",
-        "svg" : "./img/svg/PictoMolen.svg",
-        "btn" : ""
-    },
-    {
-        //Card 2:
-        "title" : "Hackathon 2023",
-        "img" : "./img/spookyCode.png",
-        "txt" : "Challenge réalisé en trois jours avec la collaboration de quatre collègues. Le site contient un jeu, dont le but est de ramasser une série d'objet avec le personnage.",
-        "svg" : "./img/svg/PictoHack.svg",
-        "btn" : ""
-    },
-    {
-        //Card 3:
-        "title" : "Pokemon Game",
-        "img" : "./img/pokemon.png",
-        "txt" : "Jeu, dont le but est de trouver les bonnes paires de Pokemons.",
-        "svg" : "./img/svg/PictosPokemons.svg",
-        "btn" : ""
-    },
-    {
-        //Card 4:
-        "title" : "Projet React: Asturias",
-        "img" : "./img/reactAsturias.png",
-        "txt" : "Site proposant un échantillon des jolis endroits à visiter, avec un tour d'horizon sur la gastronomie locale",
-        "svg" : "./img/svg/PictosAsturias.svg",
-        "btn" : ""
-    },
-    {
-        //Card 5:
-        "title" : "Projet Node.JS: API",
-        "img" : "./img/API.png",
-        "txt" : "Mini application, proposant diverses informations comme la météo, la bourse, la qualité de l'air, ... Les informations sont récupérer via différentes API.",
-        "btn" : ""
-    },
-    {
-        //Card 6:
-        "title" : "Projet React: ToDoList",
-        "img" : "./img/reactToDoList.png",
-        "txt" : "Application React peremttant d'organiser son amploi du temps en fonction de ce qui doit être fait des choses déjà accomplies",
-        "btn" : ""
-    }
-];
+//Animation balls:
+const ball = document.getElementById('logo');
+let leftPos = 0;
+let upDownPos = 0;
+let dir = -1;
+let current_rotation = 0; 
 
-//Fct randomize:
-function shuffle (DATA){
-   
-   while(l > -1){
+ball.style.position = "absolute";
 
-   let j = 0;
-   let valI = '';
-   let valJ = valI;
-   let l = DATA.length - 1;
+function ballAnimate1 () {
 
-        j = Math.floor(Math.random() * l);
-        valI = DATA[l];
-        valJ = DATA[j];
-        DATA[l] = valJ;
-        DATA[j] = valI;
-        l = l -1;
-   }
-   return DATA;
-};
-
-//Récupération des informations au niveau des cartes:
-$(document).ready(function(){
-        for(let  DATA; DATA < 6; DATA++ ){
-            $('#cardsBlock').append(`<article class="card col-12 col-md-4"> <h4 class="card-title p-4" ${DATA.title}> <img id="imgCards" ${DATA.img}> <div class="card-body"> <p id="txt" class="card-text p-1" ${DATA.txt}> <div> <img ${DATA.svg }> </article> </h4> </img> </div> </p> </div> </img>`);
-            }
-        }
-    );
-
- //Gestion des notes:
-//Récupération du span:
-$(document).ready(function(){
-    let footNotes = $('span[data-footnote]');
-    if(footNotes){
-        let footNoteDisplay = $('<ol></ol>');//Création des balises qui contiendront les notes
-        footNotes.each(function(index, element){//index = position du span sur lequel nous sommes
-            let current = $(element);
-            let footNotesTxt = current.data('footnote');
-            current.css('text-decoration', 'underline dashed')
-            .after(`<a href="#footnote-${index+1}" id="footnote-anchor-${index+1}"><sup>[${index+1}]</sup></a>`);
-            $(`<li id="footInfo-${index+1}">${footNotesTxt}</li>`)
-            .append(`<a href="#footNote-anchor-${index+1}"></a>`).appendTo(footNoteDisplay);//apppendTo permet d'injecter dans la balise :<ol>
-        });
-        //Injecter la balise <ol> dans le doc:
-        footNoteDisplay.appendTo('aside ').wrap(`<section id="footnotesDisplay"></section>`);//wrap permet d'emballer les balises <ol>
-    }
-});
-
-//Gestion slider:
-$(document).ready(function(){
-    for(let i = 0; i <15; i++){
-        $('#carouselExampleCaptions').append(`<div class="carousel-indicators" id="carousel-indic"><button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button></div>`);
+    if (leftPos == 1700) {
+        dir = 1
+    } else if 
+        (leftPos == 1000) {
+        dir = -1
     }
     
-});
+    leftPos += -2 * dir;
+    current_rotation += 1;
+    
 
-//Gestion certificats:
-$(document).ready(function(index){
-    $('<div class="carousel-indicators" id="carousel-indic">').click(`<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" ${index+1} aria-label="Slide 2"></button>`);
-});
+    ball.style.left = `${leftPos}px`;
+    ball.style.transform = 'rotate('  + current_rotation + 'deg)';
 
+    requestAnimationFrame(ballAnimate1);
+};
+
+requestAnimationFrame(ballAnimate1);
+
+//Footer map:
+
+//Create map:
+const centroid = [50.709925, 4.352603];
+const map = L.map('mapid').setView(centroid, 16.4);
+
+//Add tiles & marker:
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.marker([50.709925, 4.352603]).addTo(map);
--- assets/scripts/menu.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('#mainNav li:has(ul) > a').append('<span>&#9660;</span>');
-    $('#mainNav li:has(ul)').hover(function(){
-        $('ul', this).stop(true, true).slideToggle();//stop(true, true) = vider la liste d'attente des animations
-    });
-});
