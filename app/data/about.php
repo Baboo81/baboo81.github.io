61a6c1c0a2626e831acf23dc5b5f8359ce587cbe
Baboo81 <magicbaboo@gmail.com>
2025-04-23 14:42:23 +0200

    section : projects

---
+++ app/data/about.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\data;
+
+return [
+    'about' => [
+        'title' => 'Qui suis-je ?',
+        'p1' => 'Hello, moi c\'est Christel, développeuse web, je suis passionnée par l\'informatique depuis mes 12 ans. La création de site web m\'a permis de réunir plusieurs intérêts, en voici quelques\'uns : tou d\'abord la programmation, la créativité, la cybersécurité ainsi que la communication.',
+        'p2' => 'Après 15 années passées en pharmacie, j\'ai décidé de me former à l\'informatique ce fut un changement qui m\'a ouvert la porte vers de nouveaux horizons. Étant passionnée par la santé aussi, je me suis initié au Reiki et grâce à mon entrée dans le monde de l\'énergétique, la médecine holistique m\'a tout de suite fascinée. Actuellement je propose aussi un service de conseil en bien-être si vous êtes curieux d\'en savoir plus sur mes différentes activités, soyez les bienvenus sur mon site personnel en cliquant sur le bouton ci-dessous !',
+        'btn' => 'Aller vers le site'
+    ], 
+];
+
+?>
--- app/views/sections/home.php
+++ app/views/sections/home.php
@@ -1,5 +1,11 @@
 <?php include __DIR__ . '/../partials/header.php'; ?>
 
+<?php
+    use App\data;
+
+    $about = include __DIR__ . '/../../data/about.php';
+?>
+
 <section id="home">
     <div class="hero">
         <video autoplay loop muted playsinline class="bg-video">
@@ -23,10 +29,52 @@
                 <img class="rounded-cercle" src="img/other/mySelf2.png" alt="Photo de moi-même">
             </div>
             <div class="about2 col-md-7 d-flex aligne-items-center justify-content-center">
-                <article>
-                    <h3 class="text-center fontBleuVert mt-5">Qui suis-je</h3>
-                    <p>Hello, moi c'est Christel, développeuse web, je suis passionnée par l'informatique depuis mes 12 ans.</p>
+                <article class="text-center">
+                    <?php foreach ($about as $key1 => $aboutTemp): ?>
+                        <h3 class="text-center fontBleuVert mt-5"><?php echo $aboutTemp['title']; ?></h3>
+                            <p class="fontBleuVert mt-5"><?php echo $aboutTemp['p1']; ?></p>
+                            <p class="fontBleuVert mt-3"><?php echo $aboutTemp['p2']; ?></p>
+                        <div class="text-center my-5">
+                            <button class="button"><?php echo $aboutTemp['btn']; ?></button>
+                        </div>
+                    <?php endforeach; ?>
                 </article>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="projects">
+    <div class="row">
+        <div class="col-12">
+            <h3 class="text-center fontBleuVert mt-5">Les projets</h3>
+            <div class="d-flex align-items-center justify-content-center">
+                <!-- Cards -->   
+                <div class="card" style="width: 20rem;">
+                    <img src="img/projects/Kenko.png" class="card-img-top" alt="Image représentant la page d'accueil du site Kenko">
+                    <div class="card-body">
+                        <h5 class="card-title">Kenko</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 20rem;">
+                    <img src="img/projects/SoinsDeSoie.png" class="card-img-top" alt="Image représentant la page d'accueil du site SoinsDeSoie">
+                    <div class="card-body">
+                        <h5 class="card-title">Kenko</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 20rem;">
+                    <img src="img/projects/Souffle.png" class="card-img-top" alt="Image représentant la page d'accueil du site Souffle d'équilibre">
+                    <div class="card-body">
+                        <h5 class="card-title">Kenko</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <!-- END Cards -->  
             </div>
         </div>
     </div>
