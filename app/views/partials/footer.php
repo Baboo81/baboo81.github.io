c11527d28f7f1c84635cc3f8768f5513677b117f
Baboo81 <magicbaboo@gmail.com>
2025-04-24 10:35:13 +0200

    Footer

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,37 +1,39 @@
         <footer class="pt-4 pb-2">
             <div class="container">
                 <div class="row text-center text-md-start">
-                    <!-- Adresse -->
-                    <div class="col-12 col-md-3 mb-4">
-                        <h5>Adresse</h5>
-                        <p class="mb-0">Rue Champ Rodange, 14</p>
-                        <p>Waterloo 1410</p>
-                    </div>
-                    <!-- Carte -->
-                    <div class="col-12 col-md-3 mb-4">
-                        <h5>Carte</h5>
-                        <div id="mapid" class="w-100 rounded-3" style="height: 150px;"></div>
-                    </div>
-                    <!-- Réseaux Sociaux -->
-                    <div class="col-12 col-md-3 mb-4">
-                        <h5>Réseaux Sociaux</h5>
-                        <div class="d-flex justify-content-center justify-content-md-start gap-3">
-                            <a href="https://github.com/Baboo81">
-                                <img src="/img/pictos/Git.svg" alt="GitHub" width="32" height="32">
-                            </a>
-                            <a href="https://www.linkedin.com/in/chris-rodriguez-perez-609716100/">
-                                <img src="/img/pictos/LinkedIn.svg" alt="LinkedIn" width="32" height="32">
-                            </a>
+                        <!-- Adresse -->
+                        <div class="col-12 col-md-3 mb-4">
+                            <h5 class="my-4">Adresse</h5>
+                            <p class="mb-0">Rue Champ Rodange, 14</p>
+                            <p>Waterloo 1410</p>
+                        </div>
+                        <!-- Carte -->
+                        <div class="col-12 col-md-3 mb-4">
+                            <h5 class="my-4">Carte</h5>
+                            <div id="mapid" class="w-100 rounded-3" style="height: 150px;"></div>
+                        </div>
+                        <!-- Réseaux Sociaux -->
+                        <div class="col-12 col-md-3 mb-4">
+                            <h5 class="my-4">Réseaux Sociaux</h5>
+                            <div class="d-flex align-items-center justify-content-center gap-3">
+                                <a href="https://github.com/Baboo81">
+                                    <img src="/img/pictos/Git.svg" alt="GitHub" width="42" height="42">
+                                </a>
+                                <a href="https://www.linkedin.com/in/chris-rodriguez-perez-609716100/">
+                                    <img src="/img/pictos/LinkedIn.svg" alt="LinkedIn" width="42" height="42">
+                                </a>
+                            </div>
+                        </div>
+                        <!-- Contact -->
+                        <div class="col-12 col-md-3 mb-4">
+                            <h5 class="my-4">Contact</h5>
+                            <div class="d-flex align-items-center justify-content-center">
+                                <a href="mailto:chrisrodriguez@hotmail.be">
+                                    <img src="/img/pictos/Email.svg" alt="Mail" width="42" height="42">
+                                </a>
+                            </div>
                         </div>
                     </div>
-                    <!-- Contact -->
-                    <div class="col-12 col-md-3 mb-4">
-                        <h5>Contact</h5>
-                        <a href="mailto:chrisrodriguez@hotmail.be">
-                        <img src="/img/pictos/Email.svg" alt="Mail" width="32" height="32">
-                        </a>
-                        </div>
-                        </div>
                         <!-- Copyright -->
                         <div class="text-center pt-3 border-top mt-3">
                             <small>&copy; Kenko 2025 - Tous droits réservés.</small>
