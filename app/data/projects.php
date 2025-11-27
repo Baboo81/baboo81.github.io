10db230258c779492f75de1adc464367acdee5e7
Baboo81 <magicbaboo@gmail.com>
2025-11-27 10:47:37 +0100

    Added content

--- app/data/projects.php
+++ app/data/projects.php
@@ -10,6 +10,8 @@ return [
             'txt' => 'Le site Kenko est mon site personnel, il présente mes activités de webmaster et conseillère bien-être.',
             'btn1' => 'Run',
             'btn2' => 'Code',
+            'run' => 'https://kenko-web.be',
+            'code' => 'https://github.com/Baboo81/KenkoV4.git',
         ],
         [
             'img' => '/../../img/projects/SoinsDeSoie.png',
@@ -17,6 +19,8 @@ return [
             'txt' => 'Le site Soins De Soie est un site réalisé pour un institut de beauté.',
             'btn1' => 'Run',
             'btn2' => 'Code',
+            'run' => 'https://soinsdesoie.be',
+            'code' => 'https://github.com/Baboo81/SoinsDeSoieV3.git',
         ],
         [
             'img' => '/../../img/projects/Souffle.png',
@@ -24,6 +28,8 @@ return [
             'txt' => 'Le site Souffle d\'équilibre est un site réalisé pour une thérapeute en sophrologie.',
             'btn1' => 'Run',
             'btn2' => 'Code',
+            'run' => 'https://souffle-d-equilibre.com',
+            'code' => 'https://github.com/Baboo81/SouffleDEquilibre',
         ],
         [
             'img' => '/../../img/projects/amme.png',
@@ -31,6 +37,8 @@ return [
             'txt' => 'Le site @mme est un site pour une fiduciaire, ce site est en cours de construction.',
             'btn1' => 'Run',
             'btn2' => 'Code',
+            'run' => '',
+            'code' => 'https://github.com/Baboo81/amme.git',
         ],
         [
             'img' => '/../../img/projects/Imayah.png',
@@ -38,6 +46,8 @@ return [
             'txt' => 'Création d\'un site qui propose différents services. France-Alexandra Vigouroux est énergéticienne et propose des soins énergétiques, des méditations, des retraites, un accompagnement thérapeutique. Les soins se passent en Sicile ou à Bruxelles.',
             'btn1' => 'Run',
             'btn2' => 'Code',
+            'run' => 'https://imayah.be',
+            'code' => 'https://github.com/Baboo81/Imaya.git',
         ],
         [
             'img' => '/../../img/projects/JardiCare.png',
@@ -45,6 +55,8 @@ return [
             'txt' => 'Création d\'un site pour un entrepreneur de jardin',
             'btn1' => 'Run',
             'btn2' => 'Code',
+            'run' => 'https://jardicare.be',
+            'code' => 'https://github.com/Baboo81/jardicare.git',
         ],
     ], 
 ];
--- app/views/sections/home.php
+++ app/views/sections/home.php
@@ -1,21 +1,22 @@
 <?php include __DIR__ . '/../partials/header.php'; ?>
 
 <?php
-    use App\data;
 
-    $hero = include __DIR__ . '/../../data/hero.php';
-    $about = include __DIR__ . '/../../data/about.php';
-    $projectsData = include __DIR__ . '/../../data/projects.php';
-    $projects = $projectsData['projects'];
-    $stage = include __DIR__ . '/../../data/stage.php';
-    $skills = include __DIR__ . '/../../data/skills.php';
+use App\data;
+
+$hero = include __DIR__ . '/../../data/hero.php';
+$about = include __DIR__ . '/../../data/about.php';
+$projectsData = include __DIR__ . '/../../data/projects.php';
+$projects = $projectsData['projects'];
+$stage = include __DIR__ . '/../../data/stage.php';
+$skills = include __DIR__ . '/../../data/skills.php';
 ?>
 
 <section id="home">
     <div class="hero">
         <video autoplay loop muted playsinline class="bg-video">
             <source src="videos/background.mp4" type="video/mp4">
-            Votre navigateur ne supporte pas les vidéos !  
+            Votre navigateur ne supporte pas les vidéos !
         </video>
         <div class="hero-content">
             <?php foreach ($hero as $key2 => $heroTemp): ?>
@@ -39,8 +40,8 @@
                 <article class="text-center">
                     <?php foreach ($about as $key1 => $aboutTemp): ?>
                         <h3 class="text-center fontBleuVert mt-5"><?php echo htmlspecialchars($aboutTemp['title']); ?></h3>
-                            <p class="fontBleuVert mt-5"><?php echo htmlspecialchars($aboutTemp['p1']); ?></p>
-                            <p class="fontBleuVert mt-3"><?php echo htmlspecialchars($aboutTemp['p2']); ?></p>
+                        <p class="fontBleuVert mt-5"><?php echo htmlspecialchars($aboutTemp['p1']); ?></p>
+                        <p class="fontBleuVert mt-3"><?php echo htmlspecialchars($aboutTemp['p2']); ?></p>
                         <div class="text-center my-5">
                             <button class="button" onclick="window.location.href='https://www.kenko-web.be'">
                                 <?php echo htmlspecialchars($aboutTemp['btn']); ?>
@@ -59,24 +60,28 @@
             <h3 class="text-center fontBleuVert my-5">Les projets</h3>
         </div>
         <div class="row justify-content-center my-5">
-                <?php foreach ($projects as $project): ?>
-                    <div class="col-12 col-md-6 col-lg-4 d-flex align-items-stretch mb-4">
-                        <div class="card w-100 h-100">
-                            <img src="<?= $project['img']; ?>" class="card-img-top" alt="Image du projet <?= htmlspecialchars($project['title']); ?>">
-                            <div class="card-body d-flex flex-column">
-                                <h5 class="card-title"><?= htmlspecialchars($project['title']); ?></h5>
-                                <p class="card-text text-muted"><?= htmlspecialchars($project['txt']); ?></p>
-                                <h6 class="text-center my-3">Technologies utilisées</h6>
-                                <div class="mt-auto text-center">
-                                    <button class="button"><?= htmlspecialchars($project['btn1']); ?></button>
-                                    <button class="button"><?= htmlspecialchars($project['btn2']); ?></button>
-                                </div>
+            <?php foreach ($projects as $project): ?>
+                <div class="col-12 col-md-6 col-lg-4 d-flex align-items-stretch mb-4">
+                    <div class="card w-100 h-100">
+                        <img src="<?= $project['img']; ?>" class="card-img-top" alt="Image du projet <?= htmlspecialchars($project['title']); ?>">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title"><?= htmlspecialchars($project['title']); ?></h5>
+                            <p class="card-text text-muted"><?= htmlspecialchars($project['txt']); ?></p>
+                            <h6 class="text-center my-3">Technologies utilisées</h6>
+                            <div class="mt-auto text-center">
+                                <a href="<?= $project['run']; ?>" target="_blank" class="button">
+                                    <?= htmlspecialchars($project['btn1']); ?>
+                                </a>
+                                <a href="<?= $project['code']; ?>" target="_blank" class="button">
+                                    <?= htmlspecialchars($project['btn2']); ?>
+                                </a>
                             </div>
                         </div>
                     </div>
-                <?php endforeach; ?>
-            </div>
+                </div>
+            <?php endforeach; ?>
         </div>
+    </div>
     </div>
 </section>
 
@@ -110,58 +115,58 @@
 <section id="stage">
     <div class="">
         <div class="row stageSection">
-        <?php foreach ($stage as $key4 => $stageTemp): ?>
-            <div class="bloc1 col-md-6 d-flex aligne-items-center justify-content-center p-5">
-                <article class="text-center">
-                    <h3 class="text-center fontBleuVert mt-5"><?php echo $stageTemp['title']; ?></h3>
-                    <p class="fontBleuVert mt-5"><?php echo $stageTemp['p1']; ?></p>
-                    <p class="fontBleuVert mt-3"><?php echo $stageTemp['p2']; ?></p>
-                </article>
-            </div>
-            <div class="bloc2 col-md-6 d-flex align-items-center justify-content-center">
-                <article class="text-center p-3">
-                    <h3 class="text-center fontBleuVert mb-5"><?php echo $stageTemp['title2']; ?></h3>
+            <?php foreach ($stage as $key4 => $stageTemp): ?>
+                <div class="bloc1 col-md-6 d-flex aligne-items-center justify-content-center p-5">
+                    <article class="text-center">
+                        <h3 class="text-center fontBleuVert mt-5"><?php echo $stageTemp['title']; ?></h3>
+                        <p class="fontBleuVert mt-5"><?php echo $stageTemp['p1']; ?></p>
+                        <p class="fontBleuVert mt-3"><?php echo $stageTemp['p2']; ?></p>
+                    </article>
+                </div>
+                <div class="bloc2 col-md-6 d-flex align-items-center justify-content-center">
+                    <article class="text-center p-3">
+                        <h3 class="text-center fontBleuVert mb-5"><?php echo $stageTemp['title2']; ?></h3>
                         <div class="accordion accordion-flush shadow" id="accordionFlushExample">
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="flush-headingOne">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                    <?php echo $stageTemp['link1']; ?>
+                                        <?php echo $stageTemp['link1']; ?>
                                     </button>
                                 </h2>
                                 <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body">
-                                    <?php echo $stageTemp['accordionTxtLink1']; ?>
+                                    <div class="accordion-body">
+                                        <?php echo $stageTemp['accordionTxtLink1']; ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="flush-headingTwo">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                        <?php echo $stageTemp['link2']; ?>
+                                    </button>
+                                </h2>
+                                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">
+                                        <?php echo $stageTemp['accordionTxtLink2']; ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="flush-headingThree">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                        <?php echo $stageTemp['link3']; ?>
+                                    </button>
+                                </h2>
+                                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">
+                                        <?php echo $stageTemp['accordionTxtLink3']; ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="flush-headingTwo">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                <?php echo $stageTemp['link2']; ?>
-                                </button>
-                            </h2>
-                            <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body">
-                                    <?php echo $stageTemp['accordionTxtLink2']; ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="flush-headingThree">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                    <?php echo $stageTemp['link3']; ?>
-                                </button>
-                            </h2>
-                            <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body">
-                                    <?php echo $stageTemp['accordionTxtLink3']; ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </article>
-            </div>
-        <?php endforeach; ?>
+                    </article>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </section>
